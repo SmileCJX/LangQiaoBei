869d624dab7fb2ec77ab25ef4e429478574d1a63
SmileCJX <1092925421@qq.com>
2017-04-08 00:01:25 +0800

    将无需改成有序

--- src/adv/Adv_074.java
+++ src/adv/Adv_074.java
@@ -4,13 +4,14 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Adv_074 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int n = input.nextInt();
 		int num = 2;
-		Set<Integer> set = new HashSet<Integer>();
+		Set<Integer> set = new TreeSet<Integer>();
 		while(true){
 			if (n==1){
 				break;
